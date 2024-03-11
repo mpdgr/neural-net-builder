6b90ebe06f2880fb7364382ef22736e2a8d3c529
mpodgorski <m.podgorski@kcmrm.pl>
2024-03-11 10:28:41 +0100

    activation and testing

--- test/test_activation.py
+++ test/test_activation.py
@@ -7,7 +7,16 @@ class Test(TestCase):
         vector = [-5, 7, 0, -2, 1]
         expected = [0, 7, 0, 0, 1]
 
-        actual = __relu_vector(vector)
+        actual = relu(vector, ActivationType.FUNCTION)
+
+        self.assertEqual(expected, actual)
+
+
+    def test_relu_vector_derivative(self):
+        vector = [-5, 7, 0, -2, 1, 7, 0.2]
+        expected = [0, 1, 0, 0, 1, 1, 1]
+
+        actual = relu(vector, ActivationType.DERIVATIVE)
 
         self.assertEqual(expected, actual)
 
